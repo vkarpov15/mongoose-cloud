a91db9c91b309a479a0106b661ecdeeaa15e9f13
Valeri Karpov <val@karpov.io>
2020-05-21 12:01:22 -0400

    add lambda syntax and set connections

--- index.js
+++ index.js
@@ -3,30 +3,44 @@
 const mongoose = require('mongoose');
 
 let config = null;
+let conn = null;
 const defaultConfig = Object.freeze({});
 const defaultConnectionOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  bufferCommands: false,
+  useFindAndModify: false,
   serverSelectionTimeoutMS: 3000
 };
 
 const getConfig = () => config || defaultConfig;
+const lambdaSymbol = Symbol('mongoose-cloud:lambda');
 
 module.exports = {
   init: function(newConfig) {
+    if (config != null) {
+      return;
+    }
     config = Object.freeze(Object.assign({}, newConfig));
   },
   config: getConfig,
-  connect: function(uri, options) {
+  connect: async function(uri, options) {
+    if (conn != null) {
+      return conn;
+    }
+
     uri = uri || getConfig().uri || 'mongodb://localhost:27017/test';
     options = options || getConfig().options || defaultConnectionOptions;
 
-    return mongoose.connect(uri, options);
+    mongoose.connect(uri, options);
+    conn = mongoose.connection;
+    await conn;
   },
-  createConnection: function(uri, options) {
-    uri = uri || getConfig().uri || 'mongodb://localhost:27017/test';
-    options = options || getConfig().options || defaultConnectionOptions;
-
-    return mongoose.createConnection(uri, options);
+  // Support different syntaxes: AWS lambda syntax vs plain async functions
+  lambda: function(fn) {
+    fn[lambdaSymbol] = true;
+  },
+  isLambda: function(fn) {
+    return fn != null && fn[lambdaSymbol];
   }
 };
